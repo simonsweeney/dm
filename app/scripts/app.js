a6a331916472aea76bcd1490e8f43cb52aa5286c
simonsweeney <hello@simonsweeney.me>
2014-09-12 01:52:53 +0100

    menu and ting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ if(img.length > 0){
     function mouse(evt){
         var center_x = (offset.left) + (img.width()/2);
         var center_y = (offset.top) + (img.height()/2);
-        var mouse_x = evt.pageX; var mouse_y = evt.pageY;
+        var mouse_x = evt.clientX; 
+        var mouse_y = evt.clientY;
         var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
         var degree = (radians * (180 / Math.PI) * -1) + 90; 
         img.css('-moz-transform', 'rotate('+degree+'deg)');
